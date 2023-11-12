ac169b721808d238f9265924ee12ec4e1e4be816
dayanetiziani@hotmail.com <dayanetiziani@hotmail.com>
2023-11-12 17:07:29 -0300

    atualização para o vercel

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-//sintaxe moderna
-import express from 'express';
+//sintaxe moderna não funciona no vercell
+//import express from 'express';
 
 //exemplo de importação de biblioteca usando type:'commonjs'
 //sintaxe antiga 
-//express = require('express')
+express = require('express')
 
 const porta = 3000;
 const host = '0.0.0.0';
